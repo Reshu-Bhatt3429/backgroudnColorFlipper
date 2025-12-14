ceef3afd8ebab05d8ca84f5413cab2d77ffaa79e
Reshu <reshubhatt946@gmail.com>
2025-12-14 14:58:07 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+const btn = document.getElementById("colorBtn");
+
+function randomColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+
+}
+
+
+btn.addEventListener("click", () => {
+    document.body.style.backgroundColor = randomColor();
+})
